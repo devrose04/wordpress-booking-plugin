a5d710cc85aec2244caac23d13afd92e961bb824
devrose04 <webdevrose04@gmail.com>
2024-08-03 03:13:34 +0800

    upgrade

---
+++ template-parts/content-none.php
@@ -0,0 +1,8 @@
+<article>
+    <header>
+        <h1><?php _e('Nothing Found', 'my-new-theme'); ?></h1>
+    </header>
+    <div>
+        <p><?php _e('Sorry, no posts matched your criteria.', 'my-new-theme'); ?></p>
+    </div>
+</article>
